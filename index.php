900e68798af143b0ed536425aa7fc23fd4fc1441
yassine el maaroufi <elmaaroufi.yas@gmail.com>
2019-07-28 02:52:54 +0100

    routing ok

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require('controler/AccountsUsersManagerController.php');
 
-try {
     if (isset($_GET['action'])) {
 
         switch($_GET['action']){
@@ -50,62 +49,3 @@ try {
     {
         acceuil();
     }
-
-
-
- /*   if (isset($_GET['action'])) {
-        if ($_GET['action'] == 'listPosts') {
-            listPosts();
-        }
-        elseif ($_GET['action'] == 'post') {
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                post();
-            }
-            else {
-                
-                throw new Exception('Aucun identifiant de billet envoyé');
-            }
-        }
-        elseif ($_GET['action'] == 'addComment') {
-
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-
-                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-                }
-                else {
-                   
-                    throw new Exception('Tous les champs ne sont pas remplis !');
-                }
-            }
-            else {
-               
-                throw new Exception('Aucun identifiant de billet envoyé');
-            }
-        }
-        elseif ($_GET['action'] == 'modify') {
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                showComment();   
-            }
-            else {
-                throw new Exception('Aucun identifiant de commentaire envoyé');
-            }
-        }
-        elseif ($_GET['action'] == 'modifyComment'){
-            if(!empty($_POST['comment'])){
-               modifyComment($_POST['comment'], $_GET['id']); 
-            }
-            else {
-                throw new Exception('Tous les champs ne sont pas remplis !');
-            }
-                
-        }
-    }
-    else {
-        listPosts();
-    }*/
-}
-catch(Exception $e) {
-    $errorMessage = $e->getMessage();
-    require('view/errorView.php');
-}
